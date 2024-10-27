c098249d8cd63c3987aad9ffdc1d2bc62f5aa81b
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2024-10-27 14:55:10 +0300

    paketler yuklendi

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 ## Kutuphaneler
 
 -- npm install @react-navigation/native
--- npm install react-native-screens react-native-safe-area-context
+-- npm install react-native-screens
+-- react-native-safe-area-context
+-- npm install @react-navigation/native-stack
+-- npm install formik --save
+-- npm install yup --save
+-- npm i @ui-kitten/components @eva-design/eva react-native-svg
--- src/router/rootNavigator.jsx
+++ src/router/rootNavigator.jsx
@@ -1,14 +1,17 @@
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {ADDTASKS, TASKDETAIL, TASKS} from '../utils/routes';
+import Home from '../screens/home';
+import AddTask from '../screens/addTask';
+import TaskDetail from '../screens/taskDetail';
 
 const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Notifications" component={Notifications} />
-      <Stack.Screen name="Profile" component={Profile} />
+      <Stack.Screen name={TASKS} component={Home} />
+      <Stack.Screen name={ADDTASKS} component={AddTask} />
+      <Stack.Screen name={TASKDETAIL} component={TaskDetail} />
     </Stack.Navigator>
   );
 };
