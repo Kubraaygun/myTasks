a93bff07a6e584c733896797001a09b4f15deaef
¨kubraaygun¨ <kubraaygun@outlook.com.tr>
2024-10-27 15:18:19 +0300

    icon paketi yuklendi

---
+++ src/components/uı/floatActionButton.jsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const FloatActionButton = () => {
+  return (
+    <View>
+      <Text>floatActionButton</Text>
+    </View>
+  );
+};
+
+export default FloatActionButton;
+
+const styles = StyleSheet.create({});
--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -1,10 +1,12 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import FloatActionButton from '../../components/uı/floatActionButton';
 
 const Home = () => {
   return (
     <View>
       <Text>home</Text>
+      <FloatActionButton />
     </View>
   );
 };
